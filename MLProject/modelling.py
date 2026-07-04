989f7892be2ffcf935767869a77ebf37347f3f70
NannFth <adnandfathir123@gmail.com>
2026-07-04 14:49:33 +0700

    Fix model size for GitHub push limit

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -21,7 +21,7 @@ def main():
 
     X_train, X_test, y_train, y_test = load_data()
 
-    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model = RandomForestRegressor(n_estimators=50, max_depth=15, random_state=42)
     model.fit(X_train, y_train)
 
     score = model.score(X_test, y_test)
